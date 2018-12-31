99e75c697942b642f7ece8cdd5d2a78e4750dd8b
Dong-Seop Eah <deah@bu.edu>
2018-12-30 22:11:01 -0500

    add map route

--- flaskapp.py
+++ flaskapp.py
@@ -1,11 +1,6 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-
-
-
-
-
 @app.route("/")
 @app.route("/home")
 def home():
@@ -15,5 +10,19 @@ def home():
 def about():
     return render_template('about.html')
 
+@app.route("/map")
+def map():
+    return render_template('map.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+
+
+
+
+
+    
